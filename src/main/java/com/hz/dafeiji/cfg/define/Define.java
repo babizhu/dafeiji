15031f169d31e21446523d42d26ccab0b2037d9f
liukun <liu_kun@msn.com>
2015-01-08 17:00:47 +0800

    更新了配置表

---
+++ src/main/java/com/hz/dafeiji/cfg/define/Define.java
@@ -0,0 +1,78 @@
+package com.hz.dafeiji.cfg.define;
+
+import com.hz.dafeiji.cfg.manual.define.DefineTempletCfg;
+
+/**
+ * 常数定义，自动生成，勿改
+ *
+ * @author liukun
+ *         2015-1-8 16:56:41
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class Define{
+    static{
+        reload();
+    }
+
+    /**
+     * 装备升级所消耗的金币与能源所占比例 *
+     */
+    public static float JIN_BI_EQUIPMENT_UP = 0.1F;
+    /**
+     * 僚机升级所消耗的金币与能源所占比例 *
+     */
+    public static float JIN_BI_WING_UP = 0.1F;
+    /**
+     * 装备拆分时所获得的能源与升级所消耗能源的比例 *
+     */
+    public static float ZHUANG_BEI_CHAI_FEN = 0.1F;
+    /**
+     * 僚机被吞噬时所获得的经验与升级所消耗经验的比例 *
+     */
+    public static float LIAO_JI_TUN_SHI = 0.1F;
+    /**
+     * 装备升级时的总的参数。 *
+     */
+    public static float ZHUANG_BEI_SHENG_JI_CHANG_LIANG = 10F;
+    /**
+     * 经典模式核弹携带上限 *
+     */
+    public static int HEI_DAN_XIE_DAI_SHANG_XIAN = 9;
+
+    public static void reload(){
+        JIN_BI_EQUIPMENT_UP = getFloat( "JIN_BI_EQUIPMENT_UP" );
+        JIN_BI_WING_UP = getFloat( "JIN_BI_WING_UP" );
+        ZHUANG_BEI_CHAI_FEN = getFloat( "ZHUANG_BEI_CHAI_FEN" );
+        LIAO_JI_TUN_SHI = getFloat( "LIAO_JI_TUN_SHI" );
+        ZHUANG_BEI_SHENG_JI_CHANG_LIANG = getFloat( "ZHUANG_BEI_SHENG_JI_CHANG_LIANG" );
+        HEI_DAN_XIE_DAI_SHANG_XIAN = getInt( "HEI_DAN_XIE_DAI_SHANG_XIAN" );
+    }
+
+    private static int getInt( String s ){
+        return new Integer( get( s ) );
+    }
+
+    private static float getFloat( String s ){
+        return new Float( get( s ) );
+    }
+
+    private static Boolean getBoolean( String s ){
+        return Boolean.valueOf( get( s ) );
+    }
+
+    private static String getString( String s ){
+        String string = get( s );
+        if( string.endsWith( "\"" ) && string.startsWith( "\"" ) ) {
+            string = string.substring( 1, string.length() - 1 );
+        }
+        return string;
+    }
+
+    private static String get( String s ){
+        return DefineTempletCfg.get( s ).getValue().trim();
+    }
+
+    private static double getDouble( String s ){
+        return new Double( get( s ) );
+    }
+}
---
+++ src/main/java/com/hz/dafeiji/cfg/define/PropId.java
@@ -0,0 +1,83 @@
+package com.hz.dafeiji.cfg.define;
+
+import com.hz.dafeiji.cfg.manual.define.DefineTempletCfg;
+
+/**
+ * 常数定义，自动生成，勿改
+ *
+ * @author liukun
+ *         2015-1-8 16:56:41
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class PropId{
+    static{
+        reload();
+    }
+
+    /**
+     * 金币:游戏币，用于游戏日常消耗等。 *
+     */
+    public static int CASH_JIN_BI = 500001;
+    /**
+     * 钻石:充值获得货币. *
+     */
+    public static int CASH_ZUAN_SHI = 500002;
+    /**
+     * 总积分:总的积分数，只记录最高的 *
+     */
+    public static int ZONG_JI_FEN = 500003;
+    /**
+     * 周积分:战斗过程中获得的积分，用于排名（每周会清零） *
+     */
+    public static int ZHOU_JI_FEN = 500004;
+    /**
+     * 活动积分:用于兑换某些机甲或者是其他的道具 *
+     */
+    public static int HUO_DONG_JI_FEN = 500005;
+    /**
+     * 战斗力:玩家的整体实力体现 *
+     */
+    public static int ZHAN_DOU_LI = 500006;
+    /**
+     * 行动力:参与战斗的时候所消耗 *
+     */
+    public static int XING_DONG_LI = 500007;
+
+    public static void reload(){
+        CASH_JIN_BI = getInt( "CASH_JIN_BI" );
+        CASH_ZUAN_SHI = getInt( "CASH_ZUAN_SHI" );
+        ZONG_JI_FEN = getInt( "ZONG_JI_FEN" );
+        ZHOU_JI_FEN = getInt( "ZHOU_JI_FEN" );
+        HUO_DONG_JI_FEN = getInt( "HUO_DONG_JI_FEN" );
+        ZHAN_DOU_LI = getInt( "ZHAN_DOU_LI" );
+        XING_DONG_LI = getInt( "XING_DONG_LI" );
+    }
+
+    private static int getInt( String s ){
+        return new Integer( get( s ) );
+    }
+
+    private static float getFloat( String s ){
+        return new Float( get( s ) );
+    }
+
+    private static Boolean getBoolean( String s ){
+        return Boolean.valueOf( get( s ) );
+    }
+
+    private static String getString( String s ){
+        String string = get( s );
+        if( string.endsWith( "\"" ) && string.startsWith( "\"" ) ) {
+            string = string.substring( 1, string.length() - 1 );
+        }
+        return string;
+    }
+
+    private static String get( String s ){
+        return DefineTempletCfg.get( s ).getValue().trim();
+    }
+
+    private static double getDouble( String s ){
+        return new Double( get( s ) );
+    }
+}
