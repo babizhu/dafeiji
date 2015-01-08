e9b5c9fbe3237abf4625cbd5c3a3be62b1066a79
liukun <liu_kun@msn.com>
2015-01-08 17:09:18 +0800

    更新了配置表

---
+++ src/main/java/com/hz/dafeiji/cfg/reward/StuffTemplet.java
@@ -0,0 +1,191 @@
+package com.hz.dafeiji.cfg.reward;
+
+import org.jdom2.Element;
+
+/**
+ * 模版
+ *
+ * @author liukun
+ *         2015-1-8 16:56:41
+ */
+public class StuffTemplet{
+
+    /**
+     * id
+     */
+    private final int id;
+
+
+    /**
+     * id
+     */
+    public int getId(){
+        return id;
+    }
+
+    /**
+     * 名称
+     */
+    private final String name;
+
+
+    /**
+     * 名称
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * 显示顺序
+     */
+    private final int order;
+
+
+    /**
+     * 显示顺序
+     */
+    public int getOrder(){
+        return order;
+    }
+
+    /**
+     * 道具类型(1战斗前 2战斗中 3奖励)
+     */
+    private final int type;
+
+
+    /**
+     * 道具类型(1战斗前 2战斗中 3奖励)
+     */
+    public int getType(){
+        return type;
+    }
+
+    /**
+     * 品质
+     */
+    private final int quality;
+
+
+    /**
+     * 品质
+     */
+    public int getQuality(){
+        return quality;
+    }
+
+    /**
+     * 物品用途
+     */
+    private final String purpose;
+
+
+    /**
+     * 物品用途
+     */
+    public String getPurpose(){
+        return purpose;
+    }
+
+    /**
+     * 物品来源
+     */
+    private final String source;
+
+
+    /**
+     * 物品来源
+     */
+    public String getSource(){
+        return source;
+    }
+
+    /**
+     * 叠加数量
+     */
+    private final int addUp;
+
+
+    /**
+     * 叠加数量
+     */
+    public int getAddUp(){
+        return addUp;
+    }
+
+    /**
+     * 买价(-1表示不卖)
+     */
+    private final String sellPrice;
+
+
+    /**
+     * 买价(-1表示不卖)
+     */
+    public String getSellPrice(){
+        return sellPrice;
+    }
+
+    /**
+     * 卖价描述
+     */
+    private final String sellPriceDs;
+
+
+    /**
+     * 卖价描述
+     */
+    public String getSellPriceDs(){
+        return sellPriceDs;
+    }
+
+    /**
+     * 资源id
+     */
+    private final String resId;
+
+
+    /**
+     * 资源id
+     */
+    public String getResId(){
+        return resId;
+    }
+
+    /**
+     * 描述
+     */
+    private final String describe;
+
+
+    /**
+     * 描述
+     */
+    public String getDescribe(){
+        return describe;
+    }
+
+    public StuffTemplet( Element element ){
+        id = Integer.parseInt( element.getChildText( "id" ).trim() );
+        name = element.getChildText( "name" ).trim();
+        order = Integer.parseInt( element.getChildText( "order" ).trim() );
+        type = Integer.parseInt( element.getChildText( "type" ).trim() );
+        quality = Integer.parseInt( element.getChildText( "quality" ).trim() );
+        purpose = element.getChildText( "purpose" ).trim();
+        source = element.getChildText( "source" ).trim();
+        addUp = Integer.parseInt( element.getChildText( "addUp" ).trim() );
+        sellPrice = element.getChildText( "sellPrice" ).trim();
+        sellPriceDs = element.getChildText( "sellPriceDs" ).trim();
+        resId = element.getChildText( "resId" ).trim();
+        describe = element.getChildText( "describe" ).trim();
+
+    }
+
+    @Override
+    public String toString(){
+        return "StuffTemplet [id = " + id + ",name = " + name + ",order = " + order + ",type = " + type + ",quality = " + quality + ",purpose = " + purpose + ",source = " + source + ",addUp = " + addUp + ",sellPrice = " + sellPrice + ",sellPriceDs = " + sellPriceDs + ",resId = " + resId + ",describe = " + describe + "]";
+    }
+
+	/*自定义代码开始*//*自定义代码结束*/
+}
---
+++ src/main/java/com/hz/dafeiji/cfg/reward/StuffTempletCfg.java
@@ -0,0 +1,73 @@
+package com.hz.dafeiji.cfg.reward;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 模版配置
+ *
+ * @author liukun
+ *         2015-1-8 16:56:41
+ */
+public class StuffTempletCfg{
+    private static final Map<Integer, StuffTemplet> stuffTemplets = new HashMap<>();
+
+
+    static{
+        //init();
+
+    }
+
+    private static final String FILE = "./resource/xml/reward/stuff.xml";
+
+
+    public static void init(){
+
+        SAXBuilder builder = new SAXBuilder();
+        Document document;
+        try {
+            document = builder.build( FILE );
+            Element root = document.getRootElement();
+            List<?> list = root.getChildren( "Stuff" );
+
+            for( Object o : list ) {
+                StuffTemplet templet = new StuffTemplet( (Element) o );
+                StuffTemplet temp = stuffTemplets.put( templet.getId(), templet );
+                if( temp != null ) {
+                    throw new RuntimeException( "StuffTemplet id [" + temp.getId() + "] 重复了" );
+                }
+
+            }
+        } catch( JDOMException | IOException e ) {
+            e.printStackTrace();
+        }
+
+        System.out.println( "StuffTemplet xml配置文件解析完毕" );
+    }
+
+
+    /**
+     * 通过id获取StuffTemplet的引用
+     *
+     * @param templetId id
+     * @return 返回一个引用
+     */
+    public static StuffTemplet getStuffTempletById( int templetId ){
+        return stuffTemplets.get( templetId );
+    }
+
+	/*自定义代码开始*//*自定义代码结束*/
+
+    public static void main( String[] args ){
+
+        int id = 100001;
+        System.out.println( getStuffTempletById( id ) );
+    }
+}
