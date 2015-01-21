999253fbb4a7e374511889c7d591f3d0fd2fee22
valen <knightvivi@qq.com>
2015-01-21 10:52:38 +0800

    装备模块大改动,添加ERRORCODE

---
+++ src/main/java/com/hz/dafeiji/net/handler/phpHandler/MailGetListHandler.java
@@ -0,0 +1,18 @@
+package com.hz.dafeiji.net.handler.phpHandler;
+
+import com.alibaba.fastjson.JSONObject;
+import com.hz.dafeiji.ai.user.User;
+import com.hz.dafeiji.net.handler.IGameHandler;
+
+/**
+ * Created by Valen on 2015/1/21.
+ *
+ */
+public class MailGetListHandler implements IGameHandler {
+
+
+    @Override
+    public void run(JSONObject request, JSONObject response, User user) {
+
+    }
+}
